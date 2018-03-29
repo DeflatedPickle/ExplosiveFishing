074f558db7a7181aabcbed690f4037928b16c846
DeflatedPickle <DeflatedPickle@gmail.com>
2018-03-29 22:30:52 +0100

    Added a detector for water

--- src/main/scala/com/deflatedpickle/explosivefishing/events/ForgeEventHandler.scala
+++ src/main/scala/com/deflatedpickle/explosivefishing/events/ForgeEventHandler.scala
@@ -1,5 +1,7 @@
 package com.deflatedpickle.explosivefishing.events
 
+import net.minecraft.init.Blocks
+import net.minecraft.util.math.BlockPos
 import net.minecraftforge.event.world.ExplosionEvent
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 
@@ -10,5 +12,13 @@ class ForgeEventHandler {
 
   @SubscribeEvent
   def onExplosionEventDetonate(event: ExplosionEvent.Detonate): Unit = {
+    val x = event.getExplosion.getPosition.x
+    val y = event.getExplosion.getPosition.y
+    val z = event.getExplosion.getPosition.z
+
+    val position = new BlockPos(x, y, z)
+
+    if (event.getWorld.getBlockState(position).getBlock == Blocks.WATER) {
+    }
   }
 }
