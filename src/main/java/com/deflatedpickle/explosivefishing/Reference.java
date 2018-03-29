d9d52eca12954deee52158442e56e3915e2f446b
DeflatedPickle <DeflatedPickle@gmail.com>
2018-03-29 20:31:39 +0100

    Added reference class

---
+++ src/main/java/com/deflatedpickle/explosivefishing/Reference.java
@@ -0,0 +1,12 @@
+package com.deflatedpickle.explosivefishing;
+
+public class Reference {
+    public static final String MOD_ID = "explosivefishing";
+    public static final String NAME = "ExplosiveFishing";
+    // Versions follow this format: MCVERSION-MAJORMOD.MAJORAPI.MINOR.PATCH.
+    public static final String VERSION = "1.12.2-1.0.0.0";
+    public static final String ACCEPTED_VERSIONS = "[1.12.1, 1.12.2]";
+
+    public static final String CLIENT_PROXY_CLASS = "com.deflatedpickle.explosivefishing.proxy.ClientProxy";
+    public static final String SERVER_PROXY_CLASS = "com.deflatedpickle.explosivefishing.proxy.ServerProxy";
+}
