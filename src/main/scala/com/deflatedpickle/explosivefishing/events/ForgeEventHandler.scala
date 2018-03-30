84eb1d0c60e158c9149dd3c0dedc03c4ebaeffbb
DeflatedPickle <DeflatedPickle@gmail.com>
2018-03-30 02:12:51 +0100

    Added spawning of items

--- src/main/scala/com/deflatedpickle/explosivefishing/events/ForgeEventHandler.scala
+++ src/main/scala/com/deflatedpickle/explosivefishing/events/ForgeEventHandler.scala
@@ -1,7 +1,11 @@
 package com.deflatedpickle.explosivefishing.events
 
+import java.util.Random
+
+import net.minecraft.entity.item.EntityItem
 import net.minecraft.init.Blocks
 import net.minecraft.util.math.BlockPos
+import net.minecraft.world.storage.loot.{LootContext, LootTableList}
 import net.minecraftforge.event.world.ExplosionEvent
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 
@@ -19,6 +23,10 @@ class ForgeEventHandler {
     val position = new BlockPos(x, y, z)
 
     if (event.getWorld.getBlockState(position).getBlock == Blocks.WATER) {
+      val loot = event.getWorld.getLootTableManager.getLootTableFromLocation(LootTableList.GAMEPLAY_FISHING).generateLootForPools(new Random(), new LootContext.Builder(event.getWorld.getMinecraftServer.getWorld(0)).build())
+
+      val itemEntity = new EntityItem(event.getWorld, x, y + 1, z, loot.get(0))
+      event.getWorld.spawnEntity(itemEntity)
     }
   }
 }
